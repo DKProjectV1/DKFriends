c31360a0ff5359f8fd14876251385dc65213f738
Fridious <philippfriedhoff@gmx.de>
2018-11-16 16:03:49 +0100

    Added spigot api's

---
+++ friendsystem-spigot-api/src/main/java/ch/dkrieger/friendsystem/spigot/api/inventory/PrivateGUI.java
@@ -0,0 +1,25 @@
+package ch.dkrieger.friendsystem.spigot.api.inventory;
+
+/*
+ *
+ *  * Copyright (c) 2018 Philipp Elvin Friedhoff on 16.11.18 15:45
+ *
+ */
+
+import org.bukkit.entity.Player;
+
+public abstract class PrivateGUI extends GUI {
+
+    private Player owner;
+
+    public PrivateGUI(String name, int size, Player owner) {
+        super(name, size);
+        this.owner = owner;
+    }
+    public Player getOwner() {
+        return owner;
+    }
+    public void open(){
+        open(this.owner);
+    }
+}
